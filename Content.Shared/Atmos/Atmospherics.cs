d6785b940565f30d03ae942dc1394a3b72a04573
Redrover1760 <39284090+Redrover1760@users.noreply.github.com>
2025-06-16 07:44:34 -0400

    ports https://github.com/space-wizards/space-station-14/pull/37564

--- Content.Shared/Atmos/Atmospherics.cs
+++ Content.Shared/Atmos/Atmospherics.cs
@@ -343,7 +343,7 @@ namespace Content.Shared.Atmos
         /// <summary>
         ///     The default pressure at which pumps and powered equipment max out at, in kPa.
         /// </summary>
-        public const float MaxOutputPressure = 9000; // Mono 4500<9000
+        public const float MaxOutputPressure = 4500;
 
         /// <summary>
         ///     The default maximum speed powered equipment can work at, in L/s.
