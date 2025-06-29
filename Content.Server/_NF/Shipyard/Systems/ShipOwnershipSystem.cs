9de9dd930086ae49a4dc4959344b351bae0686b6
monolith8319 <195513600+monolith8319@users.noreply.github.com>
2025-06-29 23:30:46 +0000

    chore: automatically update REUSE headers

--- Content.Server/_NF/Shipyard/Systems/ShipOwnershipSystem.cs
+++ Content.Server/_NF/Shipyard/Systems/ShipOwnershipSystem.cs
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2025 Ark
+// SPDX-FileCopyrightText: 2025 Redrover1760
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server.GameTicking;
 using Content.Server._NF.Shipyard.Systems;
 using Content.Server.Mind;
