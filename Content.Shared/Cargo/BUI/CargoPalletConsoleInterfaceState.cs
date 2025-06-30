e92f03862480609957cf430bfa34693ec1d77f54
monolith8319 <195513600+monolith8319@users.noreply.github.com>
2025-06-30 21:45:11 +0000

    chore: automatically update REUSE headers

--- Content.Shared/Cargo/BUI/CargoPalletConsoleInterfaceState.cs
+++ Content.Shared/Cargo/BUI/CargoPalletConsoleInterfaceState.cs
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2023 Checkraze
+// SPDX-FileCopyrightText: 2025 sleepyyapril
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Robust.Shared.Serialization;
 
 namespace Content.Shared.Cargo.BUI;
