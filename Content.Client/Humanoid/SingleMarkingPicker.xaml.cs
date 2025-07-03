4c3fe7d459ac818bc8037aa335ab617f60f92ad6
Mora <46364955+TrixxedHeart@users.noreply.github.com>
2025-07-02 20:08:06 -0500

    Switch HSV to the default colorspace for character customization (#38434)
    * Made HSV default for character editor
    
    * Adds/fixes comments to HSV defaulting
    
    * Added dropbox fix, potentially cursed
    
    * Revert "Added dropbox fix, potentially cursed"
    
    This reverts commit a709883366fbee813e839742125e70844672af29.
    
    ---------
    
    Co-authored-by: TrixxedHeart <46364955+TrixxedBit@users.noreply.github.com>

--- Content.Client/Humanoid/SingleMarkingPicker.xaml.cs
+++ Content.Client/Humanoid/SingleMarkingPicker.xaml.cs
@@ -11,6 +11,9 @@ namespace Content.Client.Humanoid;
 public sealed partial class SingleMarkingPicker : BoxContainer
 {
     [Dependency] private readonly MarkingManager _markingManager = default!;
+    [Dependency] private readonly IEntityManager _entityManager = default!;
+
+    private readonly SpriteSystem _sprite;
 
     /// <summary>
     ///     What happens if a marking is selected.
@@ -226,6 +229,7 @@ public sealed partial class SingleMarkingPicker : BoxContainer
                 HorizontalExpand = true
             };
             selector.Color = marking.MarkingColors[i];
+            selector.SelectorType = ColorSelectorSliders.ColorSelectorType.Hsv; // defaults color selector to HSV
 
             var colorIndex = i;
             selector.OnColorChanged += color =>
