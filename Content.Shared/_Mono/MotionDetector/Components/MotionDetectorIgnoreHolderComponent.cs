4be8498104d57968b40b9fb5475ff42b8f9a2efa
ark1368 <189933909+ark1368@users.noreply.github.com>
2025-07-06 21:25:10 -0700

    Initial

---
+++ Content.Shared/_Mono/MotionDetector/Components/MotionDetectorIgnoreHolderComponent.cs
@@ -0,0 +1,7 @@
+namespace Content.Shared._Mono.MotionDetector.Components;
+
+/// <summary>
+/// Makes proximity detectors ignore the holder.
+/// </summary>
+[RegisterComponent]
+public sealed partial class MotionDetectorIgnoreHolderComponent : Component;
