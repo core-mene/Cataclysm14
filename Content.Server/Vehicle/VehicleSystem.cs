a24e49a11bf6a7cc705e948cd4273f64f4a2ca10
monolith8319 <195513600+monolith8319@users.noreply.github.com>
2025-07-14 17:08:25 +0000

    chore: automatically update REUSE headers

--- Content.Server/Vehicle/VehicleSystem.cs
+++ Content.Server/Vehicle/VehicleSystem.cs
@@ -1,3 +1,15 @@
+// SPDX-FileCopyrightText: 2022 DrSmugleaf
+// SPDX-FileCopyrightText: 2022 Leon Friedrich
+// SPDX-FileCopyrightText: 2022 Rane
+// SPDX-FileCopyrightText: 2023 AJCM-git
+// SPDX-FileCopyrightText: 2023 Daniil Sikinami
+// SPDX-FileCopyrightText: 2024 checkraze
+// SPDX-FileCopyrightText: 2024 metalgearsloth
+// SPDX-FileCopyrightText: 2025 HacksLua
+// SPDX-FileCopyrightText: 2025 ScyronX
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Shared.Vehicle;
 
 namespace Content.Server.Vehicle;
