da15595a9c2a06c2897030aa920f4102bfbdbb5c
starch <starchpersonal@gmail.com>
2025-07-15 17:46:52 -0400

    There we go!

---
+++ Content.Server/EntityEffects/Effects/RevertPolymorphs.cs
@@ -0,0 +1,39 @@
+using Content.Server.Polymorph.Components;
+using System.Linq;
+using Content.Server.Administration;
+using Content.Server.Polymorph.Systems;
+using Content.Server.Polymorph.Toolshed;
+using Content.Shared.EntityEffects;
+using Content.Shared.Polymorph;
+using Content.Server.Polymorph.Systems;
+using Content.Shared.Administration;
+using Robust.Shared.Prototypes;
+using Robust.Shared.Serialization.TypeSerializers.Implementations.Custom.Prototype;
+using Robust.Shared.Toolshed;
+
+namespace Content.Server.EntityEffects.Effects;
+
+public sealed partial class RevertPolymorph : EntityEffect
+{
+    /// <summary>
+    ///     What polymorph prototype is used on effect
+    /// </summary>
+    [DataField("prototype", customTypeSerializer:typeof(PrototypeIdSerializer<PolymorphPrototype>))]
+    public string PolymorphPrototype { get; set; }
+
+    protected override string? ReagentEffectGuidebookText(IPrototypeManager prototype, IEntitySystemManager entSys)
+        => Loc.GetString("reagent-effect-guidebook-revert-polymorph",
+            ("chance", Probability), ("entityname",
+                prototype.Index<EntityPrototype>(prototype.Index<PolymorphPrototype>(PolymorphPrototype).Configuration.Entity).Name));
+
+    public override void Effect(EntityEffectBaseArgs args)
+    {
+        var entityManager = args.EntityManager;
+        var uid = args.TargetEntity;
+        var polySystem = entityManager.System<PolymorphSystem>();
+
+        // Make it into a prototype
+        entityManager.EnsureComponent<PolymorphableComponent>(uid);
+        polySystem.Revert(uid);
+    }
+}
