61dc38a207cfb2638c42f86ec8f6e112907ab30a
RealSchepka <82253477+RealSchepka@users.noreply.github.com>
2025-07-15 22:50:24 +0300

    Revert a commit
    That was meant for a whole different PR and I don't even remember committing this, don't know how it ended up here. This code is not finished and I removed it

--- Content.Shared/Ame/Components/AmeFuelContainerComponent.cs
+++ Content.Shared/Ame/Components/AmeFuelContainerComponent.cs
@@ -15,12 +15,12 @@ public sealed partial class AmeFuelContainerComponent : Component
     /// <summary>
     /// The amount of fuel in the container.
     /// </summary>
-    [DataField("fuelamount", required: true), ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
-    public int FuelAmount; // mono
+   [DataField, ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
+    public int FuelAmount = 1250; // mono
 
     /// <summary>
     /// The maximum fuel capacity of the container.
     /// </summary>
-    [DataField("fuelcapacity", required: true), ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
-    public int FuelCapacity; // mono
+    [DataField, ViewVariables(VVAccess.ReadWrite), AutoNetworkedField]
+    public int FuelCapacity = 1250; // mono
 }
