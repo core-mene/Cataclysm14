4ef90dc43bde37c19e13b80fa8b8baad593833cf
monolith8319 <195513600+monolith8319@users.noreply.github.com>
2025-08-08 18:15:45 +0000

    chore: automatically update REUSE headers

--- Content.Server/_DV/Station/Components/StationPlanetSpawnerComponent.cs
+++ Content.Server/_DV/Station/Components/StationPlanetSpawnerComponent.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 starch
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Content.Server._DV.Station.Systems;
 using Content.Shared._DV.Planet;
 using Robust.Shared.Prototypes;
