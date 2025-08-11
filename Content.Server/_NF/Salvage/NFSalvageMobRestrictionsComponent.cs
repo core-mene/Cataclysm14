a8ad111424ac019735a19a793da9df31a53f5440
monolith8319 <195513600+monolith8319@users.noreply.github.com>
2025-08-11 05:35:20 +0000

    chore: automatically update REUSE headers

--- Content.Server/_NF/Salvage/NFSalvageMobRestrictionsComponent.cs
+++ Content.Server/_NF/Salvage/NFSalvageMobRestrictionsComponent.cs
@@ -1,3 +1,8 @@
+// SPDX-FileCopyrightText: 2024 Dvir
+// SPDX-FileCopyrightText: 2025 Ilya246
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
 using Robust.Shared.Prototypes;
 
 namespace Content.Server._NF.Salvage;
