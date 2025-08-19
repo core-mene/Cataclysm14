1a63dde9cf4f3657e5d4fdc86469c1d6a1daa821
tonotom1 <tonotom@proton.me>
2025-08-19 00:51:54 -0700

    Update NitriumMoveSpeedModifier.cs
    real credit

--- Content.Server/_Funkystation/Atmos/EntityEffects/Effects/NitriumMoveSpeedModifier.cs
+++ Content.Server/_Funkystation/Atmos/EntityEffects/Effects/NitriumMoveSpeedModifier.cs
@@ -1,5 +1,5 @@
 // SPDX-FileCopyrightText: 2024 SlamBamActionman
-// SPDX-FileCopyrightText: 2025 tonotom
+// SPDX-FileCopyrightText: 2025 marc-pelletier
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
