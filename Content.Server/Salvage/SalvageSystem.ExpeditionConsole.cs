f2efd1479c301726a67e79575cf3bcd24c0d0078
monolith8319 <195513600+monolith8319@users.noreply.github.com>
2025-08-20 07:17:00 +0000

    chore: automatically update REUSE headers

--- Content.Server/Salvage/SalvageSystem.ExpeditionConsole.cs
+++ Content.Server/Salvage/SalvageSystem.ExpeditionConsole.cs
@@ -9,6 +9,7 @@
 // SPDX-FileCopyrightText: 2024 metalgearsloth
 // SPDX-FileCopyrightText: 2025 Ark
 // SPDX-FileCopyrightText: 2025 GreaseMonk
+// SPDX-FileCopyrightText: 2025 Ilya246
 // SPDX-FileCopyrightText: 2025 Redrover1760
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
