madeline-celeste/Maidstation

let borgs drag-drop stuff (#2539)
* base functionality

* do

* chore: Automatically update REUSE headers

* chore: Automatically update REUSE headers

* chore: Automatically update REUSE headers

* webedit major

* fix

---------

Co-authored-by: GoobBot <uristmchands@proton.me>

Content.Goobstation.Common/Construction/SharedConstructionComponent.cs
@@ -1,0 +1,11 @@
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Robust.Shared.GameStates;
+
+namespace Content.Goobstation.Common.Construction;
+
+[Virtual, NetworkedComponent]
+public partial class SharedConstructionComponent : Component { }

Content.Goobstation.Shared/DragDrop/SharedGoobDragDropSystem.cs
@@ -1,0 +1,25 @@
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Shared.DragDrop;
+using Content.Shared.Item;
+
+namespace Content.Goobstation.Shared.DragDrop;
+
+public abstract partial class SharedGoobDragDropSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<ItemComponent, CanDragEvent>(CanDragItem);
+    }
+
+    // needed for drag-and-drop construction
+    private void CanDragItem(Entity<ItemComponent> ent, ref CanDragEvent args)
+    {
+        args.Handled = true;
+    }
+}

core-mene/Cataclysm14

Content.Shared/_Goobstation/Construction/SharedConstructionComponent.cs
@@ -1,0 +1,11 @@
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Robust.Shared.GameStates;
+
+namespace Content.Shared._Goobstation.Construction;
+
+[Virtual, NetworkedComponent]
+public partial class SharedConstructionComponent : Component { }

Content.Shared/_Goobstation/DragDrop/SharedGoobDragDropSystem.cs
@@ -1,0 +1,25 @@
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+using Content.Shared.DragDrop;
+using Content.Shared.Item;
+
+namespace Content.Shared._Goobstation.DragDrop;
+
+public abstract partial class SharedGoobDragDropSystem : EntitySystem
+{
+    public override void Initialize()
+    {
+        base.Initialize();
+
+        SubscribeLocalEvent<ItemComponent, CanDragEvent>(CanDragItem);
+    }
+
+    // needed for drag-and-drop construction
+    private void CanDragItem(Entity<ItemComponent> ent, ref CanDragEvent args)
+    {
+        args.Handled = true;
+    }
+}