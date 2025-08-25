madeline-celeste/Maidstation

borg drag-drop hotfix (#2930)
* fi x

* chore: Automatically update REUSE headers

* fix

* fix mech construction

* chore: Automatically update REUSE headers

* chore: Automatically update REUSE headers

---------

Co-authored-by: GoobBot <uristmchands@proton.me>

Content.Goobstation.Shared/DragDrop/DragDropTargetableComponent.cs
@@ -1,0 +1,12 @@
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace Content.Goobstation.Shared.DragDrop;
+
+/// <summary>
+/// Allows this entity to be the target of a drag-and-drop interact
+/// </summary>
+[RegisterComponent]
+public sealed partial class DragDropTargetableComponent : Component { }

core-mene/Cataclysm14

Content.Shared/_Goobstation/DragDrop/DragDropTargetableComponent.cs
@@ -1,0 +1,12 @@
+// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 Ilya246 <ilyukarno@gmail.com>
+//
+// SPDX-License-Identifier: AGPL-3.0-or-later
+
+namespace Content.Shared._Goobstation.DragDrop;
+
+/// <summary>
+/// Allows this entity to be the target of a drag-and-drop interact
+/// </summary>
+[RegisterComponent]
+public sealed partial class DragDropTargetableComponent : Component { }