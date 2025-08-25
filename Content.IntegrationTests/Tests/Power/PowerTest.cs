a32ab404ad5c3f56c855388224618e932e5bb66d
monolith8319 <195513600+monolith8319@users.noreply.github.com>
2025-08-25 22:29:34 +0000

    chore: automatically update REUSE headers

--- Content.IntegrationTests/Tests/Power/PowerTest.cs
+++ Content.IntegrationTests/Tests/Power/PowerTest.cs
@@ -13,6 +13,7 @@
 // SPDX-FileCopyrightText: 2024 Tayrtahn
 // SPDX-FileCopyrightText: 2024 TemporalOroboros
 // SPDX-FileCopyrightText: 2024 metalgearsloth
+// SPDX-FileCopyrightText: 2025 Coenx-flex
 // SPDX-FileCopyrightText: 2025 Redrover1760
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
