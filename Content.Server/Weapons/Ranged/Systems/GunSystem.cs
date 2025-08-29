9ff2ff2307970559fb279696442a6bf18b45c2ca
tonotom <tonotom@proton.me>
2025-08-30 00:06:01 +1000

    not how you do comments in c# you imbecile

--- Content.Server/Weapons/Ranged/Systems/GunSystem.cs
+++ Content.Server/Weapons/Ranged/Systems/GunSystem.cs
@@ -192,7 +192,7 @@ public sealed partial class GunSystem : SharedGunSystem
 
                     // Something like ballistic might want to leave it in the container still
                     if (!cartridge.DeleteOnSpawn && !Containers.IsEntityInContainer(ent!.Value))
-                        EjectCartridge(ent.Value, angle, false); #Mono: false: prevents shotguns from overlapping ejection sound
+                        EjectCartridge(ent.Value, angle, false); //Mono: false: prevents shotguns from overlapping ejection sound
 
                     Dirty(ent!.Value, cartridge);
                     break;
