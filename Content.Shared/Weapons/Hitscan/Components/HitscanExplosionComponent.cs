072eeecbddc1726df55c8bf37d1c628ba7eaa101
Avalon <jfbentley1@gmail.com>
2025-09-13 09:48:29 -0400

    It begins

---
+++ Content.Shared/Weapons/Hitscan/Components/HitscanExplosionComponent.cs
@@ -0,0 +1,17 @@
+using Robust.Shared.GameStates;
+using Robust.Shared.Prototypes;
+
+namespace Content.Shared.Weapons.Hitscan.Components;
+
+/// <summary>
+/// When this hitscan hits a target, it will explode.
+/// </summary>
+[RegisterComponent, NetworkedComponent]
+public sealed partial class HitscanExplosionComponent : Component
+{
+    /// <summary>
+    /// Explosive that will be spawned and then triggered when the hitscan hits its target.
+    /// </summary>
+    [DataField(required: true)]
+    public EntProtoId Explosive;
+};
