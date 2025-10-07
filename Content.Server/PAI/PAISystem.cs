0102713a21531dc73f97b6836bacdb1ce861584e
bitcrushing <kakodaemon@protonmail.com>
2025-10-07 20:26:50 +0100

    instrument updates from wizden

--- Content.Server/PAI/PAISystem.cs
+++ Content.Server/PAI/PAISystem.cs
@@ -2,6 +2,7 @@ using Content.Server.Ghost.Roles;
 using Content.Server.Ghost.Roles.Components;
 using Content.Server.Instruments;
 using Content.Server.Kitchen.Components;
+using Content.Shared.Instruments;
 using Content.Shared.Interaction.Events;
 using Content.Shared.Mind.Components;
 using Content.Shared.PAI;
