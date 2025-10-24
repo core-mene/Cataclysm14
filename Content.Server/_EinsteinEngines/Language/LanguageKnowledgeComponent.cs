1557f07e7abddc13000bf32f218bb90b6d7d4f5b
monolith8319 <195513600+monolith8319@users.noreply.github.com>
2025-10-24 19:57:32 +0000

    chore: automatically update REUSE headers

--- Content.Server/_EinsteinEngines/Language/LanguageKnowledgeComponent.cs
+++ Content.Server/_EinsteinEngines/Language/LanguageKnowledgeComponent.cs
@@ -1,5 +1,7 @@
-// SPDX-FileCopyrightText: 2025 CerberusWolfie <wb.johnb.willis@gmail.com>
-// SPDX-FileCopyrightText: 2025 GoobBot <uristmchands@proton.me>
+// SPDX-FileCopyrightText: 2025 CerberusWolfie
+// SPDX-FileCopyrightText: 2025 GoobBot
+// SPDX-FileCopyrightText: 2025 Redrover1760
+// SPDX-FileCopyrightText: 2025 ScyronX
 //
 // SPDX-License-Identifier: AGPL-3.0-or-later
 
