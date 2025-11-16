e31f2741ba8a52de7cf870bc5413de923b303266
monolith8319 <195513600+monolith8319@users.noreply.github.com>
2025-11-16 10:13:17 +0000

    chore: automatically update REUSE headers

--- Content.Server/_Mono/Cleanup/BaseCleanupSystem.cs
+++ Content.Server/_Mono/Cleanup/BaseCleanupSystem.cs
@@ -1,3 +1,7 @@
+// SPDX-FileCopyrightText: 2025 Ilya246
+//
+// SPDX-License-Identifier: MPL-2.0
+
 using Content.Shared._Mono.CCVar;
 using Robust.Shared.Configuration;
 using Robust.Shared.Timing;
