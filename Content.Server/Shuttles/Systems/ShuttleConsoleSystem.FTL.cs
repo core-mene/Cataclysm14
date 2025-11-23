481e192ceda1526ce6264b571c6afad8678cd778
monolith8319 <195513600+monolith8319@users.noreply.github.com>
2025-11-23 14:31:08 +0000

    chore: automatically update REUSE headers

--- Content.Server/Shuttles/Systems/ShuttleConsoleSystem.FTL.cs
+++ Content.Server/Shuttles/Systems/ShuttleConsoleSystem.FTL.cs
@@ -3,6 +3,7 @@
 // SPDX-FileCopyrightText: 2024 SlamBamActionman
 // SPDX-FileCopyrightText: 2024 metalgearsloth
 // SPDX-FileCopyrightText: 2025 Ark
+// SPDX-FileCopyrightText: 2025 Ilya246
 // SPDX-FileCopyrightText: 2025 Redrover1760
 // SPDX-FileCopyrightText: 2025 RikuTheKiller
 // SPDX-FileCopyrightText: 2025 gus
