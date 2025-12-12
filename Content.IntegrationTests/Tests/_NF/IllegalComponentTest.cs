e7af7d6b0d118e017e52accd3e1a7de692af6ea3
core-mene <fluorine2@live.fr>
2025-12-12 20:28:09 +0100

    Revert "wtf?"
    This reverts commit 3b57d7b1ccf5ffa2013ccd57bd0a509925132d70.

--- Content.IntegrationTests/Tests/_NF/IllegalComponentTest.cs
+++ Content.IntegrationTests/Tests/_NF/IllegalComponentTest.cs
@@ -1,44 +1,42 @@
-//Cataclysm14, no ships lol
+using Content.Server.Salvage.Magnet;
+using Content.Server.Solar.Components;
+using Content.Shared.Prototypes;
+using Robust.Shared.Prototypes;
 
-// using Content.Server.Salvage.Magnet;
-// using Content.Server.Solar.Components;
-// using Content.Shared.Prototypes;
-// using Robust.Shared.Prototypes;
+namespace Content.IntegrationTests.Tests._NF;
 
-// namespace Content.IntegrationTests.Tests._NF;
+[TestFixture]
+public sealed class IllegalComponentTest
+{
+    // A list of components to check all entity prototypes for.
+    private static readonly Type[] IllegalComponents =
+    {
+        typeof(SolarPanelComponent), // Frontier: use NF variant
+        typeof(SalvageMobRestrictionsComponent), // Frontier: use NF variant
+    };
 
-// [TestFixture]
-// public sealed class IllegalComponentTest
-// {
-//     // A list of components to check all entity prototypes for.
-//     private static readonly Type[] IllegalComponents =
-//     {
-//         typeof(SolarPanelComponent), // Frontier: use NF variant
-//         typeof(SalvageMobRestrictionsComponent), // Frontier: use NF variant
-//     };
+    [Test]
+    public async Task CheckServerIllegalComponents()
+    {
+        await using var pair = await PoolManager.GetServerClient();
+        var server = pair.Server;
 
-//     [Test]
-//     public async Task CheckServerIllegalComponents()
-//     {
-//         await using var pair = await PoolManager.GetServerClient();
-//         var server = pair.Server;
+        var protoManager = server.ResolveDependency<IPrototypeManager>();
 
-//         var protoManager = server.ResolveDependency<IPrototypeManager>();
+        await server.WaitAssertion(() =>
+        {
+            Assert.Multiple(() =>
+            {
+                foreach (var entity in protoManager.EnumeratePrototypes<EntityPrototype>())
+                {
+                    foreach (var component in IllegalComponents)
+                    {
+                        Assert.That(entity.HasComponent(component), Is.False, $"Entity {entity} contains illegal component {component}.");
+                    }
+                }
+            });
+        });
 
-//         await server.WaitAssertion(() =>
-//         {
-//             Assert.Multiple(() =>
-//             {
-//                 foreach (var entity in protoManager.EnumeratePrototypes<EntityPrototype>())
-//                 {
-//                     foreach (var component in IllegalComponents)
-//                     {
-//                         Assert.That(entity.HasComponent(component), Is.False, $"Entity {entity} contains illegal component {component}.");
-//                     }
-//                 }
-//             });
-//         });
-
-//         await pair.CleanReturnAsync();
-//     }
-// }
+        await pair.CleanReturnAsync();
+    }
+}
--- Content.IntegrationTests/Tests/_NF/ShipyardTests.cs
+++ Content.IntegrationTests/Tests/_NF/ShipyardTests.cs
@@ -1,111 +1,109 @@
-//Cataclysm14, no ships lol
+using System.Linq;
+using Content.Server.Cargo.Systems;
+using Content.Shared._NF.Shipyard.Prototypes;
+using Robust.Server.GameObjects;
+using Robust.Shared.EntitySerialization.Systems;
+using Robust.Shared.GameObjects;
+using Robust.Shared.Map;
+using Robust.Shared.Map.Components;
+using Robust.Shared.Prototypes;
 
-// using System.Linq;
-// using Content.Server.Cargo.Systems;
-// using Content.Shared._NF.Shipyard.Prototypes;
-// using Robust.Server.GameObjects;
-// using Robust.Shared.EntitySerialization.Systems;
-// using Robust.Shared.GameObjects;
-// using Robust.Shared.Map;
-// using Robust.Shared.Map.Components;
-// using Robust.Shared.Prototypes;
+namespace Content.IntegrationTests.Tests._NF;
 
-// namespace Content.IntegrationTests.Tests._NF;
+[TestFixture]
+public sealed class ShipyardTest
+{
+    [Test]
+    public async Task CheckAllShuttleGrids()
+    {
+        await using var pair = await PoolManager.GetServerClient();
+        var server = pair.Server;
 
-// [TestFixture]
-// public sealed class ShipyardTest
-// {
-//     [Test]
-//     public async Task CheckAllShuttleGrids()
-//     {
-//         await using var pair = await PoolManager.GetServerClient();
-//         var server = pair.Server;
+        var entManager = server.ResolveDependency<IEntityManager>();
+        var protoManager = server.ResolveDependency<IPrototypeManager>();
+        var mapLoader = entManager.System<MapLoaderSystem>();
+        var map = entManager.System<MapSystem>();
 
-//         var entManager = server.ResolveDependency<IEntityManager>();
-//         var protoManager = server.ResolveDependency<IPrototypeManager>();
-//         var mapLoader = entManager.System<MapLoaderSystem>();
-//         var map = entManager.System<MapSystem>();
+        await server.WaitPost(() =>
+        {
+            Assert.Multiple(() =>
+            {
+                foreach (var vessel in protoManager.EnumeratePrototypes<VesselPrototype>())
+                {
+                    map.CreateMap(out var mapId);
 
-//         await server.WaitPost(() =>
-//         {
-//             Assert.Multiple(() =>
-//             {
-//                 foreach (var vessel in protoManager.EnumeratePrototypes<VesselPrototype>())
-//                 {
-//                     map.CreateMap(out var mapId);
+                    try
+                    {
+                        Assert.That(mapLoader.TryLoadGrid(mapId, vessel.ShuttlePath, out var shuttle));
+                        Assert.That(shuttle.HasValue, Is.True);
+                        Assert.That(entManager.HasComponent<MapGridComponent>(shuttle.Value), Is.True);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception($"Failed to load shuttle {vessel.ShuttlePath}", ex);
+                    }
 
-//                     try
-//                     {
-//                         Assert.That(mapLoader.TryLoadGrid(mapId, vessel.ShuttlePath, out var shuttle));
-//                         Assert.That(shuttle.HasValue, Is.True);
-//                         Assert.That(entManager.HasComponent<MapGridComponent>(shuttle.Value), Is.True);
-//                     }
-//                     catch (Exception ex)
-//                     {
-//                         throw new Exception($"Failed to load shuttle {vessel.ShuttlePath}", ex);
-//                     }
+                    try
+                    {
+                        map.DeleteMap(mapId);
+                    }
+                    catch (Exception ex)
+                    {
+                        throw new Exception($"Failed to delete map {vessel.ShuttlePath}", ex);
+                    }
+                }
+            });
+        });
+        await server.WaitRunTicks(1);
+        await pair.CleanReturnAsync();
+    }
 
-//                     try
-//                     {
-//                         map.DeleteMap(mapId);
-//                     }
-//                     catch (Exception ex)
-//                     {
-//                         throw new Exception($"Failed to delete map {vessel.ShuttlePath}", ex);
-//                     }
-//                 }
-//             });
-//         });
-//         await server.WaitRunTicks(1);
-//         await pair.CleanReturnAsync();
-//     }
+    [Test]
+    public async Task NoShipyardShipArbitrage()
+    {
+        await using var pair = await PoolManager.GetServerClient();
+        var server = pair.Server;
 
-//     [Test]
-//     public async Task NoShipyardShipArbitrage()
-//     {
-//         await using var pair = await PoolManager.GetServerClient();
-//         var server = pair.Server;
+        var entManager = server.ResolveDependency<IEntityManager>();
+        var mapLoader = server.ResolveDependency<IEntitySystemManager>().GetEntitySystem<MapLoaderSystem>();
+        var map = entManager.System<MapSystem>();
+        var protoManager = server.ResolveDependency<IPrototypeManager>();
+        var pricing = server.ResolveDependency<IEntitySystemManager>().GetEntitySystem<PricingSystem>();
 
-//         var entManager = server.ResolveDependency<IEntityManager>();
-//         var mapLoader = server.ResolveDependency<IEntitySystemManager>().GetEntitySystem<MapLoaderSystem>();
-//         var map = entManager.System<MapSystem>();
-//         var protoManager = server.ResolveDependency<IPrototypeManager>();
-//         var pricing = server.ResolveDependency<IEntitySystemManager>().GetEntitySystem<PricingSystem>();
+        await server.WaitAssertion(() =>
+        {
+            Assert.Multiple(() =>
+            {
+                foreach (var vessel in protoManager.EnumeratePrototypes<VesselPrototype>())
+                {
+                    map.CreateMap(out var mapId);
+                    double appraisePrice = 0;
 
-//         await server.WaitAssertion(() =>
-//         {
-//             Assert.Multiple(() =>
-//             {
-//                 foreach (var vessel in protoManager.EnumeratePrototypes<VesselPrototype>())
-//                 {
-//                     map.CreateMap(out var mapId);
-//                     double appraisePrice = 0;
+                    Assert.That(mapLoader.TryLoadGrid(mapId, vessel.ShuttlePath, out var shuttle));
+                    Assert.That(entManager.HasComponent<MapGridComponent>(shuttle.Value), Is.True);
 
-//                     Assert.That(mapLoader.TryLoadGrid(mapId, vessel.ShuttlePath, out var shuttle));
-//                     Assert.That(entManager.HasComponent<MapGridComponent>(shuttle.Value), Is.True);
+                    pricing.AppraiseGrid(shuttle.Value, null, (uid, price) =>
+                    {
+                        appraisePrice += price;
+                    });
 
-//                     pricing.AppraiseGrid(shuttle.Value, null, (uid, price) =>
-//                     {
-//                         appraisePrice += price;
-//                     });
+                    var idealMinPrice = appraisePrice * vessel.MinPriceMarkup;
+                    var idealMaxPrice = appraisePrice * vessel.MaxPriceMarkup;
 
-//                     var idealMinPrice = appraisePrice * vessel.MinPriceMarkup;
-//                     var idealMaxPrice = appraisePrice * vessel.MaxPriceMarkup;
+                    Assert.That(vessel.Price, Is.AtLeast(idealMinPrice),
+                        $"Arbitrage possible on {vessel.ID}. Minimal price should be {idealMinPrice}, {(vessel.MinPriceMarkup - 1.0f) * 100}% over the appraise price ({appraisePrice}).");
 
-//                     Assert.That(vessel.Price, Is.AtLeast(idealMinPrice),
-//                         $"Arbitrage possible on {vessel.ID}. Minimal price should be {idealMinPrice}, {(vessel.MinPriceMarkup - 1.0f) * 100}% over the appraise price ({appraisePrice}).");
+                    if (!vessel.Classes.Contains(VesselClass.Capital))
+                    {
+                        Assert.That(vessel.Price, Is.LessThanOrEqualTo(idealMaxPrice),
+                            $"Overpriced vessel possible on {vessel.ID}. Maximum price should be {idealMaxPrice}, {(vessel.MaxPriceMarkup - 1.0f) * 100}% under the appraise price ({appraisePrice}).");
+                    }
 
-//                     if (!vessel.Classes.Contains(VesselClass.Capital))
-//                     {
-//                         Assert.That(vessel.Price, Is.LessThanOrEqualTo(idealMaxPrice),
-//                             $"Overpriced vessel possible on {vessel.ID}. Maximum price should be {idealMaxPrice}, {(vessel.MaxPriceMarkup - 1.0f) * 100}% under the appraise price ({appraisePrice}).");
-//                     }
+                    map.DeleteMap(mapId);
+                }
+            });
+        });
 
-//                     map.DeleteMap(mapId);
-//                 }
-//             });
-//         });
-
-//         await pair.CleanReturnAsync();
-//     }
-// }
+        await pair.CleanReturnAsync();
+    }
+}
